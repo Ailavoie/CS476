9ac30fe3db32493c8bd53e82c5fa2d56ecd8be33
Ashley <lavoie8a@uregina.ca>
2025-11-06 22:33:14 -0700

    2fa
    additional 2fa logic

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,7 +8,8 @@ class BaseRegisterForm(UserCreationForm):
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=False, help_text="(Optional)")
     email = forms.EmailField(required=True)
-
+    twofa = False
+    
     class Meta:
         model = User
         fields = ("email",)
