bc1c756c49737c2b783dc3a8add01eef346deaf3
NICOLAS ANTONIO FERNANDEZ MILLAR <n.fernandez07@ufromail.cl>
2025-11-14 21:16:27 -0600

    Change login redirection

--- core/views.py
+++ core/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 def home(request):
-    return render(request, 'core/home.html')
+    user = request.user
+
+    if hasattr(user, "client_profile"):
+        return render(request, "accounts/client_dashboard.html", {"user": user})
+    elif hasattr(user, "therapist_profile"):
+        return render(request, "accounts/therapist_dashboard.html", {"user": user})
+    else:
+        return redirect("accounts:register")
 
 def about(request):
     return render(request, 'core/about.html')
